7442ab49a7d44e542ff3296492d8125458df496c
John McIntosh II <jmci10@hotmail.com>
2015-08-01 03:47:25 -0400

    setup MainActivity and GameActivity UIs

---
+++ app/src/main/java/com/ver2point0/memorygame/GameActivity.java
@@ -0,0 +1,17 @@
+package com.ver2point0.memorygame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+
+    // page 184
+}
