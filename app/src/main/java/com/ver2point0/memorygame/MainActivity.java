65c01ca41649755a79a43d4d71491d0445bf9a39
John McIntosh II <jmci10@hotmail.com>
2015-08-03 20:21:27 -0400

    added high score preservation

--- app/src/main/java/com/ver2point0/memorygame/MainActivity.java
+++ app/src/main/java/com/ver2point0/memorygame/MainActivity.java
@@ -1,19 +1,37 @@
 package com.ver2point0.memorygame;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity implements
         View.OnClickListener{
 
+    // for high score
+    SharedPreferences prefs;
+    String dataName = "MyData";
+    String intName = "MyString";
+    int defaultInt = 0;
+    // for both activities
+    public static int hiScore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prefs = getSharedPreferences(dataName, MODE_PRIVATE);
+
+        // load high score or default to 0
+        hiScore = prefs.getInt(intName, defaultInt);
+
+        TextView textHiScore = (TextView) findViewById(R.id.tv_high_score_main);
+        textHiScore.setText("Hi Score: " + hiScore);
 
         Button playButton = (Button) findViewById(R.id.bt_play);
         playButton.setOnClickListener(this);
